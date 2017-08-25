776024278d1bed657b24086446f920a8da11a274
abstractioerror <abstractioerror@users.noreply.github.com>
2017-08-25 19:25:46 -0400

    rm redundant table titles

--- static/assets/js/schedule.js
+++ static/assets/js/schedule.js
@@ -8,30 +8,24 @@ $.getJSON("/static/schedule.json", function(data) {
         var $table = $('<table>', {'class': 'schedule'});
         var $schBody = $('<tbody>');
 
-        $table.append(
-            $('<thead>').append(
-                $('<tr>')
-                  .append($('<th>').text('Event'))
-                  .append($('<th>').text('Time'))
-                  .append($('<th>').text('Location'))
-            )
-        );
-
         date['schedule'].forEach(function(element) {
             var row = $('<tr>')
             row.append('<td>')
             $schBody.append(
                 $('<tr>')
-                  .append($('<td>').text(element['event']))
                   .append($('<td>').text(element['time']))
+                  .append($('<td>').text(element['event']))
                   .append($('<td>').text(element['location']))
             )
         });
 
         console.log($schBody);
-
+                 
+        var $alltable = $('<table>', {'class': 'schedule'});
+        
         $table.append($schBody);
         $('#schedule-container').append($table);
 
     });
 });
+
