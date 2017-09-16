ecfbd1cd7472ad1ffe87ad4727438d1fc3e9175f
Dafondo <austin.wang0311@gmail.com>
2017-09-16 16:55:07 -0400

    banner

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -2,6 +2,9 @@ var setBgAlpha = function(elem, alpha) {
     console.log(elem.style.backgroundColor);
 }
 
+var bannerWidth = 1920;
+var bannerHeight = 840;
+
 $(document).ready(function() {
     $('.scrollspy').scrollSpy({"scrollOffset": 0});
     $('.modal').modal();
@@ -12,6 +15,16 @@ $(document).ready(function() {
     $('#navwrapper').height($('#navbar').height());
 
     setBgAlpha(navbar, 0.1);
+    
+    // var countdownSeconds = 1509753600 - (new Date).getTime()/1000;
+    // var clock = $('#countdown').FlipClock(countdownSeconds, {
+	// 	clockFace: 'DailyCounter',
+    //     countdown: true
+    // });
+
+    // $('.flip-clock-label').remove();
+
+    // $('#home .row').css('height', $(document).width()/bannerWidth*bannerHeight + 'px');
 
     $(window).scroll(function() {
         if ($(window).scrollTop() > navtop) {
@@ -25,3 +38,7 @@ $(document).ready(function() {
         }
     });
 });
+
+// window.onresize = function() {
+//     $('#home .row').css('height', $(document).width()/bannerWidth*bannerHeight + 'px');
+// };
